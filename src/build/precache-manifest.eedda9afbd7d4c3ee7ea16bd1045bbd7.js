937d13aaeb721615fdd878fc5c9a8d653a38a385
luciano <lcniell@gmail.com>
2020-02-05 21:42:09 -0800

    My First Commit

---
+++ src/build/precache-manifest.eedda9afbd7d4c3ee7ea16bd1045bbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c65a71487e0c200ee8ce9e23d81e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97287e383403ec72a451",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9f839136ad13e11181cc",
+    "url": "/static/js/2.9dabe8b5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9dabe8b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97287e383403ec72a451",
+    "url": "/static/js/main.dde8e090.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
